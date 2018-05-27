1e6671aaea1957e0eaf21428b4c015484951195a
Ofallou <ofallou@gmail.com>
2018-05-27 04:39:08 +0200

    rs login ok

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+const jwt = require('jsonwebtoken');
+const fakeUser = {email: 'hello@world.com', password: 'welcome'};
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+const PORT = 3000;
+//routes
+const api = require('./back/routes');
+
+app.use(bodyParser.json());
+//config routes
+app.use('/api', api);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(PORT, function () {
+  console.log('Server running on localhost :3000')
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {UserdataComponent} from './userdata/userdata.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+// services
+import {TokenService} from './token.service';
+import {AuthService} from './auth.service';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    UserdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
