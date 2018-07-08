90c9e519e7b10223a7938183588746bc90d52664
Ofallou <ofallou@gmail.com>
2018-07-08 21:13:56 +0200

    loczl

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,8 +1,5 @@
-import {environment} from '../environments/environment'
 export class Config {
-  env= environment;
-   PORT = process.env.PORT || 8080;
   public static  get SOCKET_HOST(): string {
-    return 'http://localhost:PORT';
+    return 'http://localhost';
   }
 }
