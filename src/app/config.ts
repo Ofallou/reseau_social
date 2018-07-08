3fd132a4e06c5388b2df645c3a2c60ec8802452c
Ofallou <ofallou@gmail.com>
2018-07-08 19:45:11 +0200

    ENV

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,6 @@
 export class Config {
+   PORT = process.env.PORT || 8080;
   public static  get SOCKET_HOST(): string {
-    return 'http://localhost:5000/';
+    return 'http://localhost:PORT';
   }
 }
